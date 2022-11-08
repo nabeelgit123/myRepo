a26c085d391e8d711fd8eefbc618225061102ec2
nabeelgit123 <nabeelskh22@gmail.com>
2022-11-08 23:06:23 +0530

    program 2

---
+++ javaRepo/src/package1/Program2.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class Program2 {
+	public static void method1() {
+		System.out.println("called method 1");
+	}
+
+	public static void main(String[] args) {
+		method1();
+
+	}
+
+}
